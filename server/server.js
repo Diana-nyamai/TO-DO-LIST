113cf811454a102414f7aa0c81b03dc6d190a1f6
Diana-nyamai <dnyamai.dn@gmail.com>
2023-07-04 20:04:10 +0300

    Creating server

--- server/server.js
+++ server/server.js
@@ -4,4 +4,8 @@ var myServer = http.createServer((req, res) => {
     console.log("request received", req.url);
 
     res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end("hello there!");
 })
+
+myServer.listen(3000, '127.0.0.1');
+console.log("listening on port 3000");
